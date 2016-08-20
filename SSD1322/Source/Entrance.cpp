9818fca340f17f986040d460f1f907995b3613c7
Rendong Liang <penguinliong@sina.com>
2016-08-21 03:10:00 +0800

    Added test func

--- SSD1322/Source/Entrance.cpp
+++ SSD1322/Source/Entrance.cpp
@@ -7,6 +7,27 @@ using namespace LiongStudio::RaspiPlayground;
 using namespace LiongStudio::RaspiPlayground::Devices;
 using namespace std::chrono_literals;
 
+
+void printPattern(SSD1322& dev)
+{
+	unsigned char* bitmap;
+	int width, height;
+	dev.GetBitmap(bitmap, width, height);
+
+	row = height;
+	col = width;
+
+	while (row-- > 0)
+	{
+		while (col-- > 0)
+		{
+			if ((row % 2) && (col % 2))
+				bitmap[row * width + col] = 0xFF;
+		}
+	}
+	printPattern.Flush();
+}
+
 int main()
 {
 	std::cout << "App started!" << std::endl;
@@ -20,7 +41,9 @@ int main()
 	SSD1322 dev(info, 256, 64);
 	std::cout << "Printing result..." << std::endl;
 	dev.FillScreen(0xFF);
+	printPattern(dev);
 	std::cout << "Waiting for 100s to quit..." << std::endl;
 	std::this_thread::sleep_for(100000ms);
+	dev.SendCommand(SSD1322_DISPLAYOFF);
 	return 0;
 }
